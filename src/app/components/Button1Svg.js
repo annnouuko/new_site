3d1192185ac1f29e6b78997ecc823b6efe7f2ee8
Anna Kozhevnikova <kozhevnikovaanna921@gmail.com>
2025-10-29 00:48:48 +0300

    Add button SVGs and update ButtonSvg component

--- src/app/components/Button1Svg.js
+++ src/app/components/Button1Svg.js
@@ -4,12 +4,16 @@ import { useState } from "react";
 export default function ButtonSvg({ onClick, className }) {
   const [pressed, setPressed] = useState(false);
 
+  // Пути к SVG в папке public (регистр очень важен!)
+  const BUTTON1 = "/svgs/Button1.svg";
+  const BUTTON2 = "/svgs/Button2.svg";
+
   return (
     <div className={`relative ${className} flex justify-center items-center`}>
       <img
-        src={pressed ? "/svgs/Button2.svg" : "/svgs/Button1.svg"}
+        src={pressed ? BUTTON2 : BUTTON1}
         alt="Button"
-        className="w-[140px] h-auto md:w-[300px] md:h-auto cursor-pointer transition-all duration-150"
+        className="w-full h-auto md:w-[300px] md:h-[100px] cursor-pointer transition-all duration-150"
         onMouseDown={() => setPressed(true)}
         onMouseUp={() => {
           setPressed(false);
