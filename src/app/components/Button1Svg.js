e4e2f7aa4ca70547ac3efb7ac41adb1f6cba48c0
Anna Kozhevnikova <kozhevnikovaanna921@gmail.com>
2025-10-29 00:34:40 +0300

    Описание изменений

--- src/app/components/Button1Svg.js
+++ src/app/components/Button1Svg.js
@@ -7,9 +7,9 @@ export default function ButtonSvg({ onClick, className }) {
   return (
     <div className={`relative ${className} flex justify-center items-center`}>
       <img
-        src={pressed ? "/img/button2.svg" : "/img/button1.svg"}
+        src={pressed ? "/svgs/Button2.svg" : "/svgs/Button1.svg"}
         alt="Button"
-        className="w-full h-full md:w-[300px] md:h-[100px] cursor-pointer transition-all duration-150"
+        className="w-full h-auto md:w-[300px] md:h-[100px] cursor-pointer transition-all duration-150"
         onMouseDown={() => setPressed(true)}
         onMouseUp={() => {
           setPressed(false);
